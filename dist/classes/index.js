953a5eee0b4361b10a485a3eede55598d5f5a176
hay zohar <HAY12389@hotmail.com>
2021-05-26 09:55:13 +0300

    dist

--- dist/classes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./elevator"));
-__export(require("./floor"));
-__export(require("./queue"));
-//# sourceMappingURL=index.js.map
